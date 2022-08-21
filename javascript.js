582ca4be75f8caebfbfce01cfb3baf04cc8e8d05
Yoav Shalev <yoav@syqel.com>
2022-08-21 17:53:12 -0600

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/*
+# Music-Visualizer-Now-Hiring-Talented-Developers
+Wanted: Passionate Developers For Music Visualization Platform
+
+I am the founder of SYQEL.com - a music visualization platform.
+
+We are always looking to expand our development team with talented developers who are passionate about music visualization.
+
+Here is a demo of SYQEL:
+[![Watch the video](https://img.youtube.com/vi/wWzxOq7dH6g/maxresdefault.jpg)](https://youtu.be/wWzxOq7dH6g)
+
+
+If you would like to learn more - please email hello@syqel.com
+
+Please include your github link and music visualization examples
+*/
